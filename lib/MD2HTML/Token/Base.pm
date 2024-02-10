6e1d678e0d48d10752d59b5ddb2abe4dc5b1ef4c
Lycolia Rizzim <lycolia@lycolia.info>
2024-02-10 09:12:06 +0900

    Tokenのベースを作成

--- lib/MD2HTML/Token/Base.pm
+++ lib/MD2HTML/Token/Base.pm
@@ -10,8 +10,32 @@ sub new {
   my ($class, $type, $text) = @_;
   my $self = bless({
     type => $type,
+    begin => 0,
+    end => 0,
     text => $text
   }, $class);
+}
+
+# 処理開始
+sub begin_proc {
+  my $self = shift;
+  $self->{begin} = 1;
+}
+
+# 処理終了
+sub end_proc {
+  my $self = shift;
+  $self->{end} = 1;
+}
+
+sub is_begin {
+  my $self = shift;
+  return $self->{begin};
+}
+
+sub is_end {
+  my $self = shift;
+  return $self->{end};
 }
 
 1;
--- lib/MD2HTML/Token/Codefence.pm
+++ lib/MD2HTML/Token/Codefence.pm
@@ -4,10 +4,13 @@ use strict;
 use parent 'MD2HTML::Token::Base';
 
 sub new {
-  my ($class, $type, $text, $lang) = @_;
-
-  my $self = $class->SUPER::new($type, $text);
+  my ($class, $lang, $indent_len, $bquote_len) = @_;
+  my $self = $class->SUPER::new('codefence', 1, '');
+  
   $self->{lang} = $lang;
+  $self->{indent_len} = $indent_len;
+  $self->{bquote_len} = $bquote_len;
+
   return $self;
 }
 

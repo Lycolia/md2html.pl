1d243ce13d319889a7cb10c24a0242850b21b242
Lycolia Rizzim <lycolia@lycolia.info>
2024-02-12 16:58:13 +0900

    コンストラクタの作りが古いのを修正し、親クラスを変更

--- lib/MD2HTML/Token/Codefence.pm
+++ lib/MD2HTML/Token/Codefence.pm
@@ -1,11 +1,11 @@
 package MD2HTML::Token::Codefence;
 
 use strict;
-use parent 'MD2HTML::Token::Base';
+use parent 'MD2HTML::Token::Base::SingleToken';
 
 sub new {
   my ($class, $lang, $indent_len, $bquote_len) = @_;
-  my $self = $class->SUPER::new('codefence', 1, '');
+  my $self = $class->SUPER::new('codefence', '');
   
   $self->{lang} = $lang;
   $self->{indent_len} = $indent_len;
